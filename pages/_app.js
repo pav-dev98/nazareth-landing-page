f0d67c5aba6958588366807d7e0e4ac5f5274a74
pavel mansilla goyas <pavel.mansilla@gmail.com>
2024-06-03 00:02:34 -0500

    google analitycs added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import { ThemeProvider } from "next-themes";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
 import "../css/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
